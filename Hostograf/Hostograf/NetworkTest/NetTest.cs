8eb636a3afcd97ba55257a8153c928d991bb30e6
cherriod <cherriod@gmail.com>
2014-10-09 16:41:17 +0700

    Add Class Diagram

---
+++ Hostograf/Hostograf/NetworkTest/NetTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hostograf
+{
+    public class NetTest:Host
+    {
+        private ITestMethod _testMethod;
+        
+        public string TestMethod
+        {
+            get { return TestMethod; }
+            set
+            {
+                if (value.ToLower() == "tcp") _testMethod = new TcpTest();
+                else _testMethod = new IcmpTest();
+            } 
+        }
+
+        //public Host Host { get; set; }
+
+        public string Port { get; set; }
+
+        public NetTest(string TestMethod, string Hostname, string Description, string Port = "")
+            : base(Hostname, Description)
+        {
+            this.TestMethod = TestMethod;
+            this.Port = Port;
+        }
+
+        public NetTest(string TestMethod, Host host, string Port=""):base(host.Hostname, host.Description)
+        {
+            this.TestMethod = TestMethod;
+            //this.Host = Host;
+            this.Port = Port;
+        }
+
+        public void SetTestMethod(string TestMethod)
+        {
+            this.TestMethod = TestMethod;
+        }
+
+        public void TestExecute()
+        {
+            _testMethod.test(this);
+        }
+    }
+}
